78935fc38ee015f68ededba3e1dbc90c534cdea5
Aesha <aeshathakkar232@gmail.com>
2025-11-06 15:18:29 +0530

    Added JS-Course as a submodule

--- Js Course/project6/src/js/config.js
+++
@@ -1,5 +0,0 @@
-export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
-export const TIMEOUT_SEC = 30;
-export const RES_PER_PAGE = 10;
-export const KEY = '1b45fb44-68f9-41ab-88d0-8dbd9cf56b52';
-export const MODAL_CLOSE_SEC = 2.5;
